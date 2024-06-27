e150c79f22e912ac44520831dd74d67e43a6cba0
Pavel Kubica <kubicpa3@fit.cvut.cz>
2024-06-27 20:57:47 +0200

    Implement tile methods

--- src/Tile/TileContent.hpp
+++ src/Tile/TileContent.hpp
@@ -5,11 +5,14 @@
 class TileContent
 {
 public:
+    static constexpr int MINE = -1;
+
     TileContent(int number);
     bool isMine() const;
     bool isZero() const;
 
     void print(std::ostream& os) const;
+
 private:
     int number;
 };
--- src/Tile/UITile.cpp
+++ src/Tile/UITile.cpp
@@ -1,5 +1,48 @@
-//
-// Created by ABCD on 27.06.2024.
-//
 
 #include "UITile.hpp"
+
+UITile::UITile(TileContent content) : content(content), state(TileState::EMPTY)
+{}
+
+UITile::UITile(TileContent content, TileState state) : content(content), state(state)
+{}
+
+bool UITile::isRevealed() const
+{
+    return state == TileState::MINE || state == TileState::NUMBER;
+}
+
+TileContent UITile::getContent() const
+{
+    return content;
+}
+
+void UITile::reveal()
+{
+    if (content.isMine())
+        state = TileState::MINE;
+    else
+        state = TileState::NUMBER;
+}
+
+void UITile::print(std::ostream &os) const
+{
+    // TODO printing
+    switch (state)
+    {
+        case TileState::EMPTY:
+
+            break;
+        case TileState::FLAG:
+
+            break;
+        case TileState::MINE:
+
+            break;
+        case TileState::NUMBER:
+
+            break;
+        default:
+            throw std::runtime_error("unreachable branch");
+    }
+}
