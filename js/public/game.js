11839c20eb68ff5fa7c86a29bc6937a1ea060b3b
Pavel Kubica <pavel.kubica1@seznam.cz>
2024-07-09 18:41:55 +0200

    Start work on basic visuals

--- js/public/game.js
+++ js/public/game.js
@@ -5,14 +5,31 @@ const DEFAULT_MINES = 99
 function startGame()
 {
     let width = document.getElementById("widthInput").value;
-    if (width === undefined)
+    if (width == 0)
         width = DEFAULT_WIDTH;
     let height = document.getElementById("heightInput").value;
-    if (height === undefined)
+    if (height == 0)
         height = DEFAULT_HEIGHT;
     let mines = document.getElementById("mineInput").value;
-    if (mines === undefined)
+    if (mines == 0)
         mines = DEFAULT_MINES;
+    generateEmptyBoard(width, height);
 
-    // document.getElementById("game-window").innerText = "W: " + width + "H: " + height + "M: " + mines;
+}
+
+function generateEmptyBoard(width, height)
+{
+    let gameDiv = document.createElement("div");
+    gameDiv.id = "game";
+    for (let i = 0; i < width; i++)
+    {
+        for (let j = 0; j < height; j++)
+        {
+            let square = document.createElement("div");
+            square.classList.add("tile", "blank");
+            square.id = j + "_" + i;
+            gameDiv.appendChild(square);
+        }
+    }
+    document.getElementById("game-window").appendChild(gameDiv);
 }
