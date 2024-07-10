7b37288b1bde845e5667b11a7409ea76a1470386
Pavel Kubica <pavel.kubica1@seznam.cz>
2024-07-10 18:29:41 +0200

    Implement most of basic game functionality and graphics

--- js/public/game.js
+++ js/public/game.js
@@ -40,7 +40,13 @@ function generateEmptyBoard(width, height)
             let idString = toIdString(i, j);
             data[idString] = 0;
             square.id = idString;
-            square.addEventListener("click", (ev) => { revealTile(square); })
+            square.addEventListener("mouseup", (ev) =>
+                {
+                    if (ev.button === 0)
+                        revealTile(square);
+                    else if (ev.button === 2)
+                        flagTile(square);
+                })
             gameDiv.appendChild(square);
         }
     }
@@ -57,7 +63,69 @@ function revealTile(tile)
         placeMines(tile);
         firstMove = false;
     }
+    if (data[tile.id] === 0 || // revealed a zero OR
+        (!revealOne(tile) && // tile was already revealed before AND
+        data[tile.id] === getSurroundingValidTiles(tile.id).reduce( // number of flags around matches the value of the tile
+            (acc, tileId) => acc + (document.getElementById(tileId).classList.contains("flag")),
+            0)))
+    {
+        revealAdjacentRecursively(tile, true);
+    }
+}
 
+/**
+ *
+ * @param {HTMLDivElement} tile
+ * @param {boolean} force If force == true, we reveal adjacent even if current is not a 0 tile
+ */
+function revealAdjacentRecursively(tile, force = false)
+{
+    revealOne(tile);
+    if (force || data[tile.id] === 0)
+    {
+        let adjacentTiles = getSurroundingValidTiles(tile.id).filter( (tileId) => document.getElementById(tileId).classList.contains("blank"))
+        for (const adjacentTile of adjacentTiles) {
+            revealAdjacentRecursively(document.getElementById(adjacentTile))
+        }
+    }
+}
+
+function revealOne(tile)
+{
+    let retval = tile.classList.replace("blank", "num" + data[tile.id]);
+    if (data[tile.id] === MINE)
+        lose();
+    return retval;
+}
+
+function flagTile(tile)
+{
+    tile.classList.replace("blank", "flag");
+}
+
+function win()
+{
+
+}
+
+function lose()
+{
+    
+}
+
+function revealAllMines()
+{
+    for (let i = 0; i < height; i++)
+    {
+        for (let j = 0; j < width; j++)
+        {
+            let idStr = toIdString(i, j);
+            if (data[idStr] === MINE)
+                document.getElementById(idStr).classList.replace("blank", "mine");
+            else
+                document.getElementById(idStr).classList.replace("flag", "wrongflag");
+        }
+    }
 }
 
 function toIdString(x, y)
@@ -76,7 +144,6 @@ function validPosition(posStr)
 }
 
 /**
- * Places all mines, ign
  * @param {HTMLDivElement} firstTile
  */
 function placeMines(firstTile)
@@ -124,13 +191,13 @@ function getSurroundingValidTiles(tileId)
 {
     let [x, y] = fromIdString(tileId);
     return [
-             x-1 + "_" + y-1,
-             x + "_" + y-1,
-             x+1 + "_" + y-1,
-             x-1 + "_" + y,
-             x+1 + "_" + y,
-             x-1 + "_" + y+1,
-             x + "_" + y+1,
-             x+1 + "_" + y+1
+             toIdString(x-1, y-1),
+             toIdString(x, y-1),
+             toIdString(x+1, y-1),
+             toIdString(x-1, y),
+             toIdString(x+1, y),
+             toIdString(x-1, y+1),
+             toIdString(x, y+1),
+             toIdString(x+1, y+1)
            ].filter(validPosition);
 }
